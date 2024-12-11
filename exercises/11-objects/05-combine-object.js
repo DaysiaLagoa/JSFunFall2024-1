29407c3263ae7a8dd93cf580c579aeb0d423b920
Daysia Lagoa <daysia.lagoa@gmail.com>
2024-12-11 15:37:30 -0500

    changes

--- exercises/11-objects/05-combine-object.js
+++ exercises/11-objects/05-combine-object.js
@@ -13,3 +13,22 @@ const obj2 = { lastName: "Kent" }; // e.g.
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+const obj1 = { firstName: "Clark" };
+const obj2 = { lastName: "Kent" };
+
+/**
+ * Create an object called "myObject".
+ * It should be equal to "obj1" and "obj2" combined.
+ *
+ * @example
+ * console.log(myObject);
+ * // { firstName: "Clark", lastName: "Kent" }
+ *
+ * Your answer should still work when "obj1" and "obj2" are different objects.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
+
+const myObject = { ...obj1, ...obj2 };
+
+console.log(myObject); 
